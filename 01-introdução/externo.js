a609d0efa6fcbc6d814b362b105ede680ac1223d
mortari23 <matheusmortari17547@gmail.com>
2025-02-04 17:09:32 -0300

    manipulando Dom

---
+++ 01-introdução/externo.js
@@ -0,0 +1,16 @@
+
+function alternaSenha() 
+{
+    //Cole AQUI
+    let campoSenha = document.getElementById('senha')
+
+  if(campoSenha.type === 'password')
+  {
+  campoSenha.type = 'text'
+  this.innerText = 'Ocultar'
+  }else 
+  {
+  campoSenha.type = 'password'
+  this.innerText = 'Mostrar'
+  }
+}
